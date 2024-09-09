03deee15cdef0e681e7c18612f62b3488336653e
Clayton Mmbehi <mmbehiclayton@gmail.com>
2024-09-09 11:45:51 +0300

    Update AdminPanelProvider.php

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -72,7 +72,7 @@ class AdminPanelProvider extends PanelProvider
             
             ->plugins([
                 \BezhanSalleh\FilamentShield\FilamentShieldPlugin::make(),
-                ReportsPlugin::make()
+                // ReportsPlugin::make()
             ]);
     }
 }
